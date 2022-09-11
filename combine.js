e544769d5e652a2ff9b8ca04ab64e4c296f46266
alekspanteli <56685380+alekspanteli@users.noreply.github.com>
2022-09-11 22:12:59 +0300

    1 file modified

--- combine.js
+++ combine.js
@@ -1,3 +1,8 @@
+// const name = prompt("What is your name?");
+// const msg = "Hello " + name + ". Welcome to my site";
+// console.log(msg);
+
 const name = prompt("What is your name?");
-const msg = "Hello " + name + ". Welcome to my site";
+let msg = "Hello ";
+msg += name;
 console.log(msg);
